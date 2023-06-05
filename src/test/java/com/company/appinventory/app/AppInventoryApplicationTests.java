c6e188fd0642233a397ca5a208037a67776a9214
Brayan Fernandez <brayanzabaleta59@gmail.com>
2023-06-05 02:10:24 -0500

    first commit

---
+++ src/test/java/com/company/appinventory/app/AppInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.appinventory.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
